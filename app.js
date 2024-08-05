e883332ab4d08fdd34f8e1537d15815d0f273fe8
Your Name <you@example.com>
2024-08-05 11:31:15 +0530

    setup socketio

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+
+const server = http.createServer(app);
+const socketio = require("socketio");
+const io = socketio(server);
 
 app.get("/", function (req, res) {
   res.send("hello");
