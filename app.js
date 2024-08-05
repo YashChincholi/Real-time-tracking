5a3a3d224d333054bea240929d3848ee24783882
Your Name <you@example.com>
2024-08-05 11:32:09 +0530

    type error

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const app = express();
 const http = require("http");
 
 const server = http.createServer(app);
-const socketio = require("socketio");
+const socketio = require("socket.io");
 const io = socketio(server);
 
 app.get("/", function (req, res) {
   res.send("hello");
 });
 
-app.listen(3000);
+server.listen(3000);
