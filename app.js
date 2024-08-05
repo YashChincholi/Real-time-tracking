a63282df3dffdc508b560f61434dfb532e0c9c2c
Your Name <you@example.com>
2024-08-05 11:49:41 +0530

    setup view engine and static files

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const path = require("path");
 
 const server = http.createServer(app);
 const socketio = require("socket.io");
 const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
   res.send("hello");
